eb143e7740e80c0ff4b333e0ba7a55fe9d053dcf
Denis Z <zxvsdfrvsdfv@mail.ru>
2019-03-25 12:25:31 +0300

    v0.2 Connected to the database. Added authorization

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;  //fix  SQLSTATE[42000]
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //fix  SQLSTATE[42000]: Syntax error or access violation: 1071 Specified key was too long; max key length is 767 bytes (SQL: alter table `users` add unique `users_email_unique`
+        Schema::defaultStringLength(191);
     }
 }
